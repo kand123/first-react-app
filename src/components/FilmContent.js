04e6a2fb9488920e491f1d066f9c873101efef32
Kristin <kbrandtsojo@gmail.com>
2021-09-14 09:34:16 -0600

    a little styling

--- src/components/FilmContent.js
+++ src/components/FilmContent.js
@@ -1,24 +1,31 @@
-// import filmCard from './FilmCard'
-import { Card } from "@material-ui/core";
-// import { findByLabelText } from '@testing-library/dom'
+
+import { Card, Box } from "@material-ui/core";
 import "./FilmContent.css";
 
 const FilmContent = (props) => {
   const { id, title, original_title, description, date } = props;
   return (
-     
-    <Card
-      key={id}
-      className="card"
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center",}}
-    >
-      <h3>{original_title}</h3>
-      <h2>{title}</h2>
-      <h4>Released: {date}</h4>
-     
-      <p>{description}</p>
+    <Card key={id} className="card">
+      <Box
+        sx={{
+          bgcolor: "#489DD4",
+          color: "white",
+          padding: "8%",
+          textAlign: "center",
+          borderRadius: "225px 1px 175px 1px",
+          lineHeight: "25px",
+          fontWeight: "medium",
+        }}
+      >
+        {/* <Typography sx={{color: "textPrimary"}}> */}
+        <h3>{original_title}</h3>
+        <h2>{title}</h2>
+        <h4>Released: {date}</h4>
+
+        <p>{description}</p>
+        {/* </Typography> */}
+      </Box>
     </Card>
-   
   );
 };
 
