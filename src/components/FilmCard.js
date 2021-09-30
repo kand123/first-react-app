da1b553e49a7d588c2cf8f78e9acb1aa0867a5a2
Kristin Andrews <46536351+kand123@users.noreply.github.com>
2021-09-29 23:37:02 -0600

    added a little styling to cards

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,64 +1,32 @@
-import * as React from 'react'
-import ghibliData from './../data/GhibliData';
-import FilmContent from './FilmContent'
-// import Modal from '@mui/material/Modal'
-import Box from '@mui/material/Box'
-// import Typography from '@mui/material/Typography'
+import * as React from "react";
+import ghibliData from "./../data/GhibliData";
+import FilmContent from "./FilmContent";
+import Box from "@mui/material/Box";
 
 
 const FilmCard = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        marginTop: 7,
+      }}
+    >
+      {ghibliData.map((film) => {
+        return (
+          <FilmContent
+            id={film.id}
+            title={film.title}
+            original_title={film.original_title}
+            description={film.description}
+            date={film.release_date}
+          />
+        );
+      })}
+    </Box>
+  );
+};
 
-  // const [open, setOpen] = React.useState(false)
-  // const handleOpen = () => setOpen(true)
-  // const handleClose = () => setOpen(false)
- 
-//   const modalBoxStyle = {
-//    position: 'absolute',
-//    top: '50%',
-//    left: '50%',
-//   padding: '1%',
-//   width:'50%',
-//    transform: 'translate(-50%, -50%)',
-//    // width: 400,
-//    // height: 400,
-//    bgcolor: 'white',
-//    // border: '2px solid #000',
-//    boxShadow: 24,
-//    p: 4,
-//  } 
-
-
-return (
-    <Box 
-    sx={{
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'center',
-      marginTop:7,
-    }}
-  >
-{ghibliData.map((film) => {
-    return(
-       
-        <FilmContent
-       
-        id = {film.id}
-        title = {film.title}
-        original_title={film.original_title}
-        description={film.description}
-        date={film.release_date}
-        
-        />
-   
- 
-)
-
-
-})}
-
-</Box>
- )   
- 
-}
-
-export default FilmCard
+export default FilmCard;
--- src/components/FilmContent.js
+++ src/components/FilmContent.js
@@ -1,115 +1,119 @@
-
-import * as React from 'react'
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-
-import CardContent from '@mui/material/CardContent'
-import Typography from '@mui/material/Typography'
-// import { flexbox } from '@mui/system';
-import CloseIcon from '@mui/icons-material/Close';
-import Modal from '@mui/material/Modal'
-import Box from '@mui/material/Box'
-import Rating from '@mui/material/Rating';
-
-
+import * as React from "react";
+import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
 
 const FilmContent = (props) => {
   const { id, title, original_title, description, date } = props;
 
-
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-
-  
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleModalClose = () => {
-    handleOpen()
-  }
+    handleOpen();
+  };
 
   const modalBoxStyle = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-   padding: '1.5%',
-   paddingBottom: '3%',
-   width:'50%',
-    transform: 'translate(-50%, -50%)',
-    // width: 400,
-    // height: 400,
-    bgcolor: 'white',
-    // border: '2px solid #000',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    padding: "1.5%",
+    paddingBottom: "3%",
+    width: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "white",
     boxShadow: 24,
     p: 4,
-  } 
- 
+  };
 
-  
-    const [value, setValue] = React.useState();
+const buttonDivStyle = {
+// height: "100%",
+// display: "flex",
+// flexFlow: 'column',
 
- 
+// marginBottom: "15%",
+position: 'relative', 
+bottom: 30,
 
+}
+
+
+  const [value, setValue] = React.useState();
 
   return (
-    <Card className="card" 
-      
+    <Card
+      className="card"
+      sx={{
+        bgcolor: "white",
+        width: "300px",
+        height: "300px",
+        color: "black",
+        padding: "2%",
+        margin: "1%",
+        textAlign: "center",
+        lineHeight: "25px",
+        fontWeight: "medium",
+      }}
+    >
+      <CardContent
+        key={id}
         sx={{
           bgcolor: "white",
-          width: '300px',
-          height: '300px',
           color: "black",
-          padding: "2%",
-          margin: "1%",
-          textAlign: "center",
-          lineHeight: "25px",
-          fontWeight: "medium",
-          
+         height:"100%",
+         display: 'flex',
+         flexFlow: 'column',
+         justifyContent: 'space-between',
         }}
       >
-        <CardContent key={id}
-        sx={{
-          bgcolor: 'white',
-          color: 'black',
-        }}
+       <div>
+        <Typography variant="h7">{original_title}</Typography>
+        <Typography variant="h5" component="h2" mb="2%">
+          {title}
+        </Typography>
+        <Typography
+          variant="p"
+          component="h4"
+          position="50%"
+          fontWeight="normal"
         >
-          <Typography variant="h7">
-      
-        {original_title}
-        </Typography >
-        <Typography variant ="h5" component = "h2" mb="2%">
-        {title}
+          {date}
         </Typography>
-        <Typography variant ="p" component="h4" position="50%" fontWeight='normal' >
-        {date}
-        </Typography>
-        <Button variant="outlined" onClick={handleModalClose}>Plot Summary</Button>
-       
-       <Modal key={id} open={open} onClose={handleClose}>
-        <Box bgcolor="white" style={modalBoxStyle} >
-          <Typography variant="h5">{title}</Typography>
-          <Typography variant="h6">Plot Summary</Typography>
-          <Typography variant = 'p' >{description}</Typography>
-          <CloseIcon sx={{position: "absolute", top: 20, right: 20,}} onClick={handleClose}></CloseIcon>
+        </div>
+
+        <Modal key={id} open={open} onClose={handleClose}>
+          <Box bgcolor="white" style={modalBoxStyle}>
+            <Typography variant="h5">{title}</Typography>
+            <Typography variant="h6">Plot Summary</Typography>
+            <Typography variant="p">{description}</Typography>
+            <CloseIcon
+              sx={{ position: "absolute", top: 20, right: 20 }}
+              onClick={handleClose}
+            ></CloseIcon>
+          </Box>
+        </Modal>
+        <div style={buttonDivStyle}>
+        <Button variant="outlined" sx={{bgcolor: 'lightblue', border: "lightblue", color: "black", mb:'2%'}} onClick={handleModalClose}>
+          Plot Summary
+        </Button>
+        <Box>
+          <Typography component="legend">My Rating</Typography>
+          <Rating sx={{p:0, m:0}}
+            name="simple-controlled"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          />
         </Box>
-
-      </Modal>
-
-
-<Box >
- <Typography component="legend">My Rating</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      </Box>
-
-
-
-     </CardContent>
-
+        </div>
+      </CardContent>
     </Card>
   );
 };
