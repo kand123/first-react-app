8604929987a9df3de3f8a8f120ea4a5ccd46a243
Kristin Andrews <46536351+kand123@users.noreply.github.com>
2021-11-04 21:50:18 -0600

    fixed dependency problem

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import FilmCard from './pages/FilmCard'
 import RatingsButton from './components/RatingsButton'
 import {GhibliContextProvider} from './contexts/GhibliContext'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import Welcome from './pages/Welcome'
+// import { Route, Switch, Redirect } from 'react-router-dom'
+// import Welcome from './pages/Welcome'
 
 function App() {
 
@@ -12,28 +12,11 @@ function App() {
    
     <div className="main"> 
       <h1>Studio Ghibli Films</h1>
-      <GhibliContextProvider>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/welcome" />
-        </Route>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/films">
-          
-        <FilmCard/>
-        
-        </Route>
-        <Route path="*">
-       return <h1>Page Not Found</h1>
-        </Route>
-        </Switch>
+    
       
-      {/* <GhibliContextProvider> */}
+      <GhibliContextProvider>
       <RatingsButton/>
-      {/* <FilmCard /> */}
-      {/* </GhibliContextProvider> */}
+      <FilmCard /> 
       </GhibliContextProvider>
     </div>
    
