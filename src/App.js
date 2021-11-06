180750c0ad132638a1be1dbbc47a7cf62ab0caa9
Kristin Andrews <46536351+kand123@users.noreply.github.com>
2021-11-05 18:34:32 -0600

    fixed basic routing problem

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import FilmCard from './pages/FilmCard'
 import RatingsButton from './components/RatingsButton'
 import {GhibliContextProvider} from './contexts/GhibliContext'
-// import { Route, Switch, Redirect } from 'react-router-dom'
-// import Welcome from './pages/Welcome'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Welcome from './pages/Welcome'
 
 function App() {
 
@@ -12,12 +12,27 @@ function App() {
    
     <div className="main"> 
       <h1>Studio Ghibli Films</h1>
-    
-      
       <GhibliContextProvider>
       <RatingsButton/>
+    <Switch>
+<Route path="/" exact>
+  <Redirect to="/welcome" />
+  </Route>
+  <Route path="/welcome">
+    <Welcome/>
+    </Route>
+      <Route path="/films">
+        <FilmCard/>
+        </Route>
+      <Route path="*">
+        <h1>Page Not Found</h1>
+        </Route>
+      
       <FilmCard /> 
+      </Switch>
       </GhibliContextProvider>
+
+
     </div>
    
   );
